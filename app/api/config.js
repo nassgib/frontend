d738d05f2c645b231649ae0d7cddb38a9da69c73
nassgib <149698460+nassgib@users.noreply.github.com>
2024-05-28 22:33:17 +0300

    /

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://students-nastya-back.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://students-nastya-back.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
